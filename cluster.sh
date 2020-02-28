4f2e04ab8f3c7dd62a5e513164afcb6a5fa0b9b6
Bharat Kunwar <brtknr@bath.edu>
2020-02-28 10:19:05 +0000

    Move auto-approve flag to action

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 TFVARS=${1:-coreos}
-ACTION=${2:-apply}
+ACTION=${2:-apply -auto-approve}
 pushd `dirname $0`
-terraform $ACTION -var-file=$TFVARS -auto-approve
+terraform $ACTION -var-file=$TFVARS
 popd
